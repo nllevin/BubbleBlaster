3acbe467a12f36fb1f8a065c000ddb19b72e4aee
nllevin <50681582+nllevin@users.noreply.github.com>
2019-11-21 11:53:50 -0500

    Refactor many responsibilities from Game class to Level class

--- src/game.js
+++ src/game.js
@@ -1,32 +1,8 @@
 import Level from "./level";
-import Player from "./player";
-import Bubble from "./bubble";
 
 export default class BubbleBlaster {
   constructor(canvas) {
     this.ctx = canvas;
-    this.level = new Level();
-    this.player = new Player(canvas);
-    this.bubbles = [
-      new Bubble(canvas, {
-        color: "RED", 
-        size: 3, 
-        x_dir: -1, 
-        x_init: canvas.canvas.width * 0.25,
-        y_init: 210,
-        y_vel_init: 0.01
-      }),
-      new Bubble(canvas, {
-        color: "BLUE", 
-        size: 3, 
-        x_dir: 1,
-        x_init: canvas.canvas.width * 0.75,
-        y_init: 210,
-        y_vel_init: 0.01
-      })
-    ];
-    
-    this.preloaded = false;
     this.images = {};
     this.loadImages([
       "background", 
@@ -37,77 +13,6 @@ export default class BubbleBlaster {
       "spikes",
       "wall"
     ]);
-    this.frozen = false;
-    
-    this.play = this.play.bind(this);
-    this.play();
-  }
-  
-  animate() {
-    this.level.animate(
-      this.ctx, 
-      this.images.background, 
-      this.images.spikes,
-      this.images.wall
-    );
-    if (this.player.harpoon) {
-      this.player.harpoon.animate(this.ctx, this.images.items);
-    }
-    this.bubbles.forEach(bubble => bubble.animate(this.ctx, this.images.bubbles));
-    this.player.animate(this.ctx, {
-      right: this.images.players, 
-      left: this.images.mirrorPlayers,
-      dying: this.images.players
-    });
-  }
-
-  checkCollisions() {
-    if (this.bubbles.some(bubble => bubble.collidesWith(this.player))) {
-      this.frozen = true;
-      this.player.orientation = "dying";
-      this.player.spriteIdx = 0;
-      this.lossSequenceFrame = 1;
-    } else {
-      let bubblesCopy = this.bubbles.slice();
-      let newBubbles = [];
-
-      bubblesCopy.forEach(bubble => {
-        if (bubble.y_pos > 32) {
-          newBubbles.push(bubble);
-        }
-      });
-
-      if (this.player.harpoon) {
-        const poppedBubbleIdx = this.bubbles.findIndex(bubble => {
-          return bubble.collidesWith(this.player.harpoon);
-        });
-        if (poppedBubbleIdx !== -1) {
-          this.player.harpoon = null;
-          newBubbles.splice(poppedBubbleIdx, 1);
-
-          const poppedBubble = this.bubbles[poppedBubbleIdx];
-          if (poppedBubble.size) {
-            newBubbles = newBubbles.concat([
-              new Bubble(this.ctx, {
-                color: poppedBubble.color,
-                size: poppedBubble.size - 1,
-                x_dir: -1,
-                x_init: poppedBubble.x_pos + 0.375 * poppedBubble.radius(),
-                y_init: poppedBubble.y_pos + 0.5 * poppedBubble.radius()
-              }),
-              new Bubble(this.ctx, {
-                color: poppedBubble.color,
-                size: poppedBubble.size - 1,
-                x_dir: 1,
-                x_init: poppedBubble.x_pos + 0.375 * poppedBubble.radius(),
-                y_init: poppedBubble.y_pos + 0.5 * poppedBubble.radius()
-              })
-            ]);
-          }
-        }
-      }
-      this.bubbles = newBubbles;
-    }
   }
 
   loadImages(imageNames) {
@@ -117,7 +22,7 @@ export default class BubbleBlaster {
       image.onload = () => {
         this.images[imageName] = image;
         if (Object.keys(this.images).length === imageNames.length) {
-          this.preloaded = true;
+          this.run();
         }
       };
 
@@ -125,24 +30,7 @@ export default class BubbleBlaster {
     });
   }
 
-  play() {
-    window.requestAnimationFrame(this.play);
-    
-    if (!this.preloaded) {
-      // display loading bar
-    } else if (!this.frozen) {
-      this.update();
-      this.animate();
-      this.checkCollisions();
-    } else {
-      this.player.deathThroes(this.lossSequenceFrame);
-      this.animate();
-      this.lossSequenceFrame = this.lossSequenceFrame + 1;
-    }
-  }
-
-  update() {
-    this.player.update();
-    this.bubbles.forEach(bubble => bubble.update());
+  run() {    
+    this.level = new Level(this.ctx, this.images);
   }
 }
--- src/level.js
+++ src/level.js
@@ -1,3 +1,6 @@
+import Player from "./player";
+import Bubble from "./bubble";
+
 const CONSTANTS = {
   BACKGROUND_WIDTH: 384,
   BACKGROUND_HEIGHT: 208,
@@ -8,35 +11,142 @@ const CONSTANTS = {
 };
 
 export default class Level {
-  constructor() {
+  constructor(canvas, images) {
+    this.ctx = canvas;
+    this.images = images;
+
+    this.player = new Player(canvas);
+    this.bubbles = [
+      new Bubble(canvas, {
+        color: "RED",
+        size: 3,
+        x_dir: -1,
+        x_init: canvas.canvas.width * 0.25,
+        y_init: 210,
+        y_vel_init: 0.01
+      }),
+      new Bubble(canvas, {
+        color: "BLUE",
+        size: 3,
+        x_dir: 1,
+        x_init: canvas.canvas.width * 0.75,
+        y_init: 210,
+        y_vel_init: 0.01
+      })
+    ];
+
+    this.frozen = false;
+
+    this.play = this.play.bind(this);
+    this.play();
   }
 
-  animate(ctx, backgroundSprites, spikesSprites, wallSprite) {
-    const wall = ctx.createPattern(wallSprite, "repeat");
-    ctx.fillStyle = wall;
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  animate() {
+    this.drawBackground();
+    if (this.player.harpoon) {
+      this.player.harpoon.animate(this.ctx, this.images.items);
+    }
+    this.bubbles.forEach(bubble => bubble.animate(this.ctx, this.images.bubbles));
+    this.player.animate(this.ctx, {
+      right: this.images.players,
+      left: this.images.mirrorPlayers,
+      dying: this.images.players
+    });
+  }
 
-    ctx.drawImage(
-      backgroundSprites, 
+  checkCollisions() {
+    if (this.bubbles.some(bubble => bubble.collidesWith(this.player))) {
+      this.frozen = true;
+      this.player.orientation = "dying";
+      this.player.spriteIdx = 0;
+      this.lossSequenceFrame = 1;
+    } else {
+      let bubblesCopy = this.bubbles.slice();
+      let newBubbles = [];
+
+      bubblesCopy.forEach(bubble => {
+        if (bubble.y_pos > 32) {
+          newBubbles.push(bubble);
+        }
+      });
+
+      if (this.player.harpoon) {
+        const poppedBubbleIdx = this.bubbles.findIndex(bubble => {
+          return bubble.collidesWith(this.player.harpoon);
+        });
+        if (poppedBubbleIdx !== -1) {
+          this.player.harpoon = null;
+          newBubbles.splice(poppedBubbleIdx, 1);
+
+          const poppedBubble = this.bubbles[poppedBubbleIdx];
+          if (poppedBubble.size) {
+            newBubbles = newBubbles.concat([
+              new Bubble(this.ctx, {
+                color: poppedBubble.color,
+                size: poppedBubble.size - 1,
+                x_dir: -1,
+                x_init: poppedBubble.x_pos + 0.375 * poppedBubble.radius(),
+                y_init: poppedBubble.y_pos + 0.5 * poppedBubble.radius()
+              }),
+              new Bubble(this.ctx, {
+                color: poppedBubble.color,
+                size: poppedBubble.size - 1,
+                x_dir: 1,
+                x_init: poppedBubble.x_pos + 0.375 * poppedBubble.radius(),
+                y_init: poppedBubble.y_pos + 0.5 * poppedBubble.radius()
+              })
+            ]);
+          }
+        }
+      }
+      this.bubbles = newBubbles;
+    }
+  }
+
+  drawBackground() {
+    const wall = this.ctx.createPattern(this.images.wall, "repeat");
+    this.ctx.fillStyle = wall;
+    this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+    this.ctx.drawImage(
+      this.images.background, 
       8, 8, 
       CONSTANTS.BACKGROUND_WIDTH, CONSTANTS.BACKGROUND_HEIGHT, 
       CONSTANTS.LEVEL_X, CONSTANTS.LEVEL_Y, 
       CONSTANTS.LEVEL_WIDTH, CONSTANTS.LEVEL_HEIGHT
     );
     
-    ctx.drawImage(
-      spikesSprites,
+    this.ctx.drawImage(
+      this.images.spikes,
       111, 110,
       526, 16,
       CONSTANTS.LEVEL_X + 2 * 8.5, CONSTANTS.LEVEL_Y + 2 * 8,
       (CONSTANTS.LEVEL_WIDTH - 4 * 8) / 2, 10
     );
-    ctx.drawImage(
-      spikesSprites,
+    this.ctx.drawImage(
+      this.images.spikes,
       111, 110,
       526, 16,
       CONSTANTS.LEVEL_X + 2 * 8 + (CONSTANTS.LEVEL_WIDTH - 4 * 8) / 2, CONSTANTS.LEVEL_Y + 2 * 8,
       (CONSTANTS.LEVEL_WIDTH - 4 * 8) / 2, 10
     );
+  }
+
+  play() {
+    window.requestAnimationFrame(this.play);
+    if (!this.frozen) {
+      this.update();
+      this.animate();
+      this.checkCollisions();
+    } else {
+      this.player.deathThroes(this.lossSequenceFrame);
+      this.animate();
+      this.lossSequenceFrame = this.lossSequenceFrame + 1;
+    }
+  }
+
+  update() {
+    this.player.update();
+    this.bubbles.forEach(bubble => bubble.update());
   }
 }
